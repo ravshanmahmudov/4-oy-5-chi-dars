98418bc64433344c341e27c73162088bc19266aa
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-05-30 01:43:53 +1400

    masala

---
+++ js/index.js
@@ -0,0 +1,89 @@
+// 1chi masala
+
+
+// let a= 1 ;
+// let b = 50 ;
+// for (let i = a ; i <= b; i+=2) {
+//     console.log(i**2);
+// }
+
+// 2chi masala
+
+// let a =1;
+// let b = 10;
+
+// for (let i = a ; i <= b; i++) {
+//     console.log(i^i);
+// }
+
+
+// 3chi masala
+
+
+// let n = +prompt("")
+// for (let i =1; i<=20; i++){
+//     let factorial =1;
+//     for (let n =1; n<=i; n++){
+//     factorial *=n
+//     }
+//     console.log(`factorial ${i}  hisoblandi ${factorial}`);
+// }
+
+
+// 4chi masala
+
+// let a = 1;
+// let b = 30;
+// let sum = 0;
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum + i**2;
+// }console.log(sum);
+
+// 5chi masala
+
+// for (let i =1; i<=20; i++){
+//     let c =0;
+//     for(let j=1; j<=i; j++){
+//     if(i%j==0){
+//         c++
+// }
+//     }
+// if(c==2){
+//     console.log(i);
+// }
+// }
+
+
+
+
+
+// 6chi masala
+
+// let b =100;
+// for ( let i =1 ; i*2<b; i++){
+// console.log(i);
+// }
+
+// 7chi masala
+// let a =1;
+// let b =100;
+// for ( let i =a ; i<=b; i++){
+//     if(i % 7==0 && i % 5==0){
+//     console.log(i);
+//     }
+// }
+
+
+
+
+// 8chi masala
+
+// let n = +prompt("");
+// let m = 0;
+// for( let i = 1; i <= n; i++){
+//    if (n % i == 0 ){
+//     m=m+i
+//     console.log(m);
+// }
+// }
+
